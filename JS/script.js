f3c1e6c632c5c3fc1444c6070ee3d5fd3f2fd895
PlayerC26 <64438085+PlayerC26@users.noreply.github.com>
2023-04-01 00:38:07 -0500

    punto 2 terminado
    igual que el primero, hay un error al compilar, sin embargo da la respuesta que se necesita

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,18 @@ $(document).ready(function() {
         $("#txtArea").val("El texto tiene: "+ contador +" caracteres");
     });
 });
+
+$(document).ready(function() {
+    $("#caractBtn").click(function() {
+      var textoIngresado = $("#inputTxT2").val();
+      var mensaje = "";
+      
+      if (!isNaN(textoIngresado)) { // si el valor es numérico
+        mensaje = "El valor ingresado es tipo numérico";
+      } else { // si el valor es texto
+        mensaje = "El valor ingresado es de tipo texto";
+      }
+      
+      $("#txtArea2").val(mensaje);
+    });
+  });
