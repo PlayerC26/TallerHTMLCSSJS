fcfbf601f6922cba936e4309ed259e010dbcdf94
PlayerC26 <64438085+PlayerC26@users.noreply.github.com>
2023-04-01 12:24:11 -0500

    punto 4 acabado
    falta ponerle color a las respuestas requeridas

--- JS/script.js
+++ JS/script.js
@@ -29,11 +29,32 @@ $(document).ready(function() {
       var mensaje = "";
       
       if (edad>17) { // verifica que sea mayor a 17 para saber si es mayor de edad
-        mensaje = "es mayo de edad";
+        mensaje = "es mayor de edad";
       } else { // si no, es menor de edad
         mensaje = "es menor de edad";
       }
       
       $("#txtArea3").val(nombre + " " + mensaje);
+    });
+  });
+
+  $(document).ready(function() {
+    $("#calNotBtn").click(function() {
+      var nombre = $("#inputTxT4").val();
+      var nota1 = parseFloat($("#inputTxT42").val());
+      var nota2 = parseFloat($("#inputTxT43").val());
+      var nota3 = parseFloat($("#inputTxT44").val());
+
+      var mensaje = "";
+      
+      var res = (nota1 + nota2 + nota3) / 3;
+
+      if (res >= 3) { // verifica que sea mayor a 17 para saber si es mayor de edad
+        mensaje = "Felicitaciones "+ nombre + " su nota es " + res;
+      } else { // si no, es menor de edad
+        mensaje = "Lo siento "+ nombre + " su nota es " + res;
+      }
+      
+      $("#txtArea4").val(mensaje);
     });
   });
